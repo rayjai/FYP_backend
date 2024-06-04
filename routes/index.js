d13e27e3c4fc763b9c7689dd23176b97275af687
Rayng218 <manheing218@gmail.com>
2024-06-04 16:27:14 +0800

    basic setup

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
 
+const { connectToDB, ObjectId } = require("../utils/db");
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
